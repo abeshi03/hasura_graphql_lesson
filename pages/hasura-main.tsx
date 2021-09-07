edcaca945a817ff1a5354579f0bc5d9a347a2835
abeshi03 <k_913411@yahoo.co.jp>
2021-09-08 00:42:14 +0900

    add to local fetch

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -6,7 +6,9 @@ import { GetUsersQuery } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
 
 const FetchMain: VFC = () => {
-  const { data, error, loading } = useQuery<GetUsersQuery>(GET_USERS)
+  const { data, error, loading } = useQuery<GetUsersQuery>(GET_USERS, {
+    fetchPolicy: "network-only"
+  })
   if (loading)
     return (
       <Layout title="Hasura fetchPolicy">
